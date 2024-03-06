420e3e6800ba51611c924a1af6cab7959f64da40
Richard-Vesely <risa.ves@gmail.com>
2024-03-06 08:32:26 +0100

    Modified script and added CNAME

--- sync-and-push.py
+++ sync-and-push.py
@@ -1,6 +1,11 @@
 import subprocess
 import shutil
 import os
+# Configure paths
+blog_path = r"C:\Users\risav\personal_blog_inc_full_project"
+jekyll_shared_content_path = r"C:\Users\risav\personal_blog_inc_full_project\jekyll-shared-content"
+_site_path = os.path.join(blog_path, "_site")
+
 
 def run_command(commands, cwd=None):
     """Run a command using subprocess."""
@@ -28,6 +33,24 @@ def copy_directory_contents(src_dir, dest_dir):
                 shutil.copy2(s, d)
         except Exception as e:
             print(f"Failed to copy {s} to {d}. Error: {e}")
+
+def run_jekyll_build():
+    """Run the Jekyll build command with specific configuration files."""
+    os.chdir(blog_path)
+    command = f"jekyll build --config _config.yml, config-lang.yml"
+    run_command(command, cwd=blog_path)
+    print("Jekyll build completed.")
+
+def copy_cname_to_site(blog_path, site_path):
+    """Copy the CNAME file from the blog directory to the _site directory."""
+    cname_src = os.path.join(blog_path, "CNAME")
+    cname_dest = os.path.join(site_path, "CNAME")
+    if os.path.exists(cname_src):
+        shutil.copy2(cname_src, cname_dest)
+        print("CNAME file copied to _site directory.")
+    else:
+        print("CNAME file does not exist in the blog directory.")
+
 # Function to perform git add, commit, and push in a given directory
 def git_commit_push(directory, commit_message):
     """Run git add, commit, and push commands in the specified directory."""
@@ -40,10 +63,7 @@ def git_commit_push(directory, commit_message):
     except subprocess.CalledProcessError as e:
         print(f"An error occurred while pushing to GitHub: {e}")
 
-# Configure paths
-blog_en_path = r"C:\Users\risav\personal_blog_inc_full_project"
-jekyll_shared_content_path = r"C:\Users\risav\personal_blog_inc_full_project\jekyll-shared-content"
-_site_path = os.path.join(blog_en_path, "_site")
+
 
 # Shared directories and files to sync
 shared_content = [
@@ -58,15 +78,18 @@ shared_content = [
 commit_message = input("Enter commit message\n")
 
 # Add, commit, and push changes in the blog-en directory
-git_commit_push(blog_en_path, commit_message)
+git_commit_push(blog_path, commit_message)
 
+
+run_jekyll_build()
+copy_cname_to_site(blog_path, _site_path)
 git_commit_push(_site_path, commit_message)
 
 
 
 # Step B: Sync shared content and push in the jekyll-shared-content
 for content in shared_content:
-    source_path = os.path.join(blog_en_path, content)
+    source_path = os.path.join(blog_path, content)
     dest_path = os.path.join(jekyll_shared_content_path, content)
     if os.path.isdir(source_path):
         copy_directory_contents(source_path, dest_path)
